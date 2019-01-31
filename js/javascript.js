f59e333eb85331b771bfb36872002d4346c3ede0
bluewins <31166454+pinconspiracy@users.noreply.github.com>
2019-01-31 12:21:00 +0100

    autoConnect test

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,15 @@
-function checkInd() {
+var device_id = "DE:4B:7D:E6:AD:65";
+ble.autoConnect(device_id, connectCallback, disconnectCallback);
 
+function checkInd() {
+  messageInput.value = "Tjek ind";
 }
 
 function checkUd() {
+  messageInput.value = "Tjek ud";
+}
 
+function sendData() { // send data to Arduino
+	var data = stringToBytes(messageInput.value);
+	ble.writeWithoutResponse(ConnDeviceId, blue.serviceUUID, blue.txCharacteristic, data, onSend, onError);
 }
