e060e7ca0679f70f43da0677351bafe3c57364ce
Clevons <npwh@outlook.com>
2019-02-03 20:30:37 +0100

    leget med ultrasonisk sensor

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -163,11 +163,12 @@ void loop(void)
       } */
     int c = ble.read();
     
+    
     Serial.print((char)c);
       if ((char)c == '1') {
          for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo.write(pos = 180);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
  
