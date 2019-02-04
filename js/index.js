0b98d2bb1bec496eeaa44d33a1b283014f5f4530
bluewins <31166454+pinconspiracy@users.noreply.github.com>
2019-02-04 21:53:29 +0100

    oprydning

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-// ASCII only. Denne funktion er overflødig. Rest fra Lones kode.
-function bytesToString(buffer) {
-    return String.fromCharCode.apply(null, new Uint8Array(buffer));
-}
-
 // ASCII only. Denne funktion bruger i kommunikation med bommen. Ændrer vores
 // "1" til bytes, så beskeden kan transmitteres.
 function stringToBytes(string) {
@@ -15,14 +10,14 @@ function stringToBytes(string) {
 
 // Variabler som bluetooth bruger til identificere kommunikation.
 var blue ={
-	serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+    serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
     txCharacteristic: '6e400002-b5a3-f393-e0a9-e50e24dcca9e', // transmit is from the phone's perspective
     rxCharacteristic: '6e400003-b5a3-f393-e0a9-e50e24dcca9e'  // receive is from the phone's perspective
 }
 
 // Variabler der bruges til at identificere basestationen og lave en liste
 // over resultatet af en scanning
-var ConnDeviceId;
+var ConnDeviceId = 'DE:4B:7D:E6:AD:65';
 var deviceList =[];
 var inRange = 0;
 
@@ -32,23 +27,13 @@ var inRange = 0;
 // se https://www.w3schools.com/jsref/met_document_addeventlistener.asp
 function onLoad(){
 	document.addEventListener('deviceready', onDeviceReady, false);
-  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
-      console.log('file system open: ' + fs.name);
-      fs.root.getFile("newPersistentFile.txt", { create: true, exclusive: false }, function (fileEntry) {
-
-          console.log("fileEntry is file?" + fileEntry.isFile.toString());
-          // fileEntry.name == 'someFile.txt'
-          // fileEntry.fullPath == '/someFile.txt'
-          writeFile(fileEntry, null);
-      }, onErrorCreateFile);
-  }, onErrorLoadFs);
    // bleDeviceList.addEventListener('touchstart', conn, false); // assume not scrolling
 }
 
 // Denne funktion køres efter onLoad funktionen.
 function onDeviceReady(){
 	refreshDeviceList();
-  console.log(cordova.file);
+  connect();
 }
 
 // Denne funktion køres efter on DeviceReady. Vi skal bruge denne funktion til
@@ -81,29 +66,16 @@ function onDiscoverDevice(device){
 	  }
 }
 
-//BEACON TEST SLUT
-
-// Denne funktion er overflødig. Rest fra Lones kode.
-function conn(){
-	document.getElementById("debugDiv").innerHTML =""; // empty debugDiv
-	ConnDeviceId = 'DE:4B:7D:E6:AD:65';
-	document.getElementById("debugDiv").innerHTML += "<br>"+ConnDeviceId+"<br>"; //for debug:
-	ble.connect(ConnDeviceId, onConnect, onConnError);
-}
-
 // Denne funktion forbinder appen automatisk med vores blufruit, der
 // kontrollerer bommen. Undersøg om den kan rykkes op til onLoad funktionen.
-function connTest(){
-  ConnDeviceId = 'DE:4B:7D:E6:AD:65';
+function connect(){
   // https://github.com/don/cordova-plugin-ble-central#autoconnect
   ble.autoConnect(ConnDeviceId, onConnect, onError);
 }
 
-
  // Denne funktion giver besked hvis der er forbundet til bommen.
 function onConnect(){
 	document.getElementById("statusDiv").innerHTML = " Status: Connected";
-	document.getElementById("bleId").innerHTML = ConnDeviceId;
 // https://github.com/don/cordova-plugin-ble-central#startnotification
 	ble.startNotification(ConnDeviceId, blue.serviceUUID, blue.rxCharacteristic, onData, onError);
 }
@@ -114,36 +86,11 @@ function onConnError(){
 	document.getElementById("statusDiv").innerHTML = " Status: Disonnected";
 }
 
-// Denne funktion bruger vi ikke. Rest fra Lones kode.
- function onData(data){ // data received from Arduino
-	document.getElementById("receiveDiv").innerHTML =  "Received: " + bytesToString(data) + "<br/>";
-}
-
-// Denne funktion bruger vi ikke. Rest fra Lones kode.
-function data(txt){
-	messageInput.value = txt;
-}
-
 // Denne funktion giver bommen besked om at den skal åbne
 function sendData() { // send data to Arduino
 	var data = stringToBytes('1');
 // https://github.com/don/cordova-plugin-ble-central#writewithoutresponse
 	ble.writeWithoutResponse(ConnDeviceId, blue.serviceUUID, blue.txCharacteristic, data, onSend, onError);
-}
-
-// Denne funktion giver besked om at bommen har modtaget beskeden. Skal muligvis fjernes.
-function onSend(){
-	document.getElementById("sendDiv").innerHTML = "Registreret: " + "<br/>";
-}
-
-// Denne funktion bruger vi ikke. Rest fra Lones kode.
-function disconnect() {
-	ble.disconnect(deviceId, onDisconnect, onError);
-}
-
-// Denne funktion bruger vi ikke. Rest fra Lones kode.
-function onDisconnect(){
-	document.getElementById("statusDiv").innerHTML = "Status: Disconnected";
 }
 
 // Denne funktion giver fejlbeskeder på skærmen.
@@ -153,11 +100,11 @@ function onError(reason)  {
 
 function indTjek() {
   if (inRange == 1) {
-  var url = "http://api.thingspeak.com/update?api_key=QS1B4C4WUR75QAWZ&field1=50";
-  var target = '_blank';
-  var options = "location=no,hidden = yes"
-  var ref = cordova.InAppBrowser.open(url, target, options);
-  ref.close();
+    var url = "http://api.thingspeak.com/update?api_key=QS1B4C4WUR75QAWZ&field1=50";
+    var target = '_blank';
+    var options = "location = no,hidden = yes"
+    var ref = cordova.InAppBrowser.open(url, target, options);
+    ref.close();
   }
   else {
     document.getElementById("BEACON").innerHTML = "Uden for rækkevidde <br> BEACON1 <br>";
