441104e7db3fc0ce23fdc4598fd92556267e0550
bluewins <31166454+pinconspiracy@users.noreply.github.com>
2019-02-06 12:33:41 +0100

    tilføjes kommentarer til js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// ASCII only. Denne funktion bruger i kommunikation med bommen. Ændrer vores
+//  Kun ASCII. Denne funktion bruger i kommunikation med bommen. Ændrer vores
 // "1" til bytes, så beskeden kan transmitteres.
 function stringToBytes(string) {
     var array = new Uint8Array(string.length);
@@ -15,25 +15,19 @@ var blue ={
     rxCharacteristic: '6e400003-b5a3-f393-e0a9-e50e24dcca9e'  // receive is from the phone's perspective
 }
 
-// Variabler der bruges til at identificere basestationen og lave en liste
-// over resultatet af en scanning
-var ConnDeviceId;
-
-// Denne funktion køres efter on DeviceReady. Vi skal bruge denne funktion til
-// at scanne efter vores beacon. Skal optimeres så den scanner i et interval
+// Denne funktion køres når man trykker på "Check Ind". Scanner efter enheder
 function scanCheckInd(){
   document.getElementById("statusMsgDiv").innerHTML = "";
   if (cordova.platformId === 'android') { // Android filtering is broken
     ble.scan([], 5, sendCheckInd, onError);
   }
   else {
-  //alert("Disconnected");
   ble.scan([blue.serviceUUID], 5, sendCheckInd, onError);
   }
 }
 
-// Denne funktion benytter listen fra refreshDeviceList og giver besked når
-// vore beacon er fundet. Skal sætte en timestamp funktion igang.
+// Denne funktion køres efter scanCheckInd. Laver et reqest, hvis det korrekte
+// device id er fundet
 function sendCheckInd(device){
 
 	if (device.id == 'EF:1E:94:22:B3:E8') {
@@ -46,6 +40,7 @@ function sendCheckInd(device){
   }
 }
 
+// Denne funktion køres når man trykker på "Check Ind". Scanner efter enheder
 function scanCheckUd(){
       document.getElementById("statusMsgDiv").innerHTML = "";
   if (cordova.platformId === 'android') { // Android filtering is broken
@@ -57,8 +52,8 @@ function scanCheckUd(){
   }
 }
 
-// Denne funktion benytter listen fra refreshDeviceList og giver besked når
-// vore beacon er fundet. Skal sætte en timestamp funktion igang.
+// Denne funktion køres efter scanCheckUd. Laver et reqest, hvis det korrekte
+// device id er fundet
 function sendUdtjek(device){
 	if (device.id == 'EF:1E:94:22:B3:E8') {
     var url = "https://api.thingspeak.com/update?api_key=4IH5YM5BNORPHLI9&field1=0";
@@ -70,15 +65,13 @@ function sendUdtjek(device){
 	  }
 }
 
-// Denne funktion forbinder appen automatisk med vores blufruit, der
-// kontrollerer bommen. Undersøg om den kan rykkes op til onLoad funktionen.
+// Denne funktion køres når man trykker på "Åben bom". Forbinder til bommens
+// bluetooth. Autoconnect er ikke nødvendigt, men vi har det for en sikkerheds
+// skyld. Kalder enten onConnect hvis der er forbindelse eller onConnError
+// hvis der ikke er
 function conn(){
-  ConnDeviceId = 'DE:4B:7D:E6:AD:65';
+  var ConnDeviceId = 'DE:4B:7D:E6:AD:65';
   ble.autoConnect(ConnDeviceId, onConnect, onError);
-}
-
-function onSend(){
-  document.getElementById("statusMsgDiv").innerHTML = "";
 }
 
  // Denne funktion giver besked hvis der er forbundet til bommen.
@@ -93,11 +86,17 @@ function onConnError(){
 	document.getElementById("statusDiv").innerHTML = " Status: Disonnected";
 }
 
-// Denne funktion giver bommen besked om at den skal åbne
-function sendData() { // send data to Arduino
+// Denne funktion giver bommen besked om at den skal åbne. Sender "1" til
+// til bommen. Når beskeden er sendt kalder den onSend, hvis fejl onError.
+function sendData() {
 	var data = stringToBytes('1');
 	ble.writeWithoutResponse(ConnDeviceId, blue.serviceUUID, blue.txCharacteristic, data, onSend, onError);
   document.getElementById("statusMsgDiv").innerHTML = "Bom åbner";
+}
+
+// Hvis beskeden til bommen er sendt korrekt. Bliver status fjernet.
+function onSend(){
+  document.getElementById("statusMsgDiv").innerHTML = "";
 }
 
 // Denne funktion giver fejlbeskeder på skærmen.
@@ -105,6 +104,8 @@ function onError(reason)  {
 	alert("ERROR: " + reason); // real apps should use notification.alert
 }
 
+// Denne funktion blliver kaldt når oversigts knappen trykkes.
+// Linker til Thingspeak kanalen, som registerer arbejdstid.
 function oversigt(){
   var url = "https://thingspeak.com/channels/696323/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15";
   var target = '_blank';
